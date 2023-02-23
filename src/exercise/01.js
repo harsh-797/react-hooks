a53c1760180278a610750e77c9df6ba3fd997d9e
Harshit Sinha <harshit.sinha@sprinklr.com>
2023-02-23 11:11:52 +0000

    exercise1 completed

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,27 +3,27 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({initialName}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {name ? <strong>Hello {name}</strong> : 'Please type your names'}
     </div>
   )
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName={'Harshit'} />
 }
 
 export default App
